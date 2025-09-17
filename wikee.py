31495d84903bcd1ffe28587b695c3af23c51b177
40_JluTpoB_vodku <delevoperrazvedka@gmail.com>
2025-09-18 02:29:59 +0500

    test commit

--- wikee.py
+++ wikee.py
@@ -1,9 +1,8 @@
-import subprocess
 import sys
 import os
 import time
 
-def clear_screen():
+def clear():
     if os.name == 'nt':
         os.system('cls')
     else:
@@ -18,29 +17,29 @@ GRAY = '\033[90m'
 CYAN = '\033[96m'
 RESET = '\033[0m'
 
-def show_menu():
-    print(f"\n{GRAY}=== МЕНЮ ВЫБОРА ЖАЛОБ ==={RESET}\n{BLUE}1. просто снос\n2. порнография\n3. незаконные действия\n4. информация о программе\n5. доп. жалобы\n\n9. ранк жалоб\n{RED}0. выход{RESET}")
+def menu():
+    print(f"\n{GREEN}―――――――――――――――――――\n МЕНЮ ВЫБОРА ЖАЛОБ\n―――――――――――――――――――{RESET}\n{BLUE}[1]  просто снос\n[2]  порнография\n[3]  незаконные действия\n[4]  информация о программе\n[5]  доп. жалобы\n\n[9]  ранк жалоб\n{RED}[0]  выход{RESET}")
 
-def show_about():
-    clear_screen()
-    print(f"{GRAY}=== ИНФОРМАЦИЯ О ПРОГРАММЕ ==={RESET}\n{BLUE}Разработчик: 40_JluTpoB_vodku\nВерсия: BETA 0.2\nНазвание: wikee\nНазначение: помощь в составлении жалоб,снос{RESET}")
+def info():
+    clear()
+    print(f"{GRAY}――――――――――――――――――――――――\n ИНФОРМАЦИЯ О ПРОГРАММЕ\n――――――――――――――――――――――――{RESET}\n{BLUE}Разработчик: 40_JluTpoB_vodku\nВерсия: BETA 0.2\nНазвание: wikee\nНазначение: помощь в составлении жалоб,снос{RESET}")
     enters()
 
-def show_rank():
-    clear_screen()
-    print(f"{GRAY}=== РАНГИ ЖАЛОБ ===\n{RESET}в разработке")
+def rank():
+    clear()
+    print(f"{GRAY}―――――――――――――――――――\n    РАНГИ ЖАЛОБ\n―――――――――――――――――――\n{RESET}в разработке")
     enters()
 
-def show_dop():
-    clear_screen()
-    print(f"{GRAY}=== ДОП. ЖАЛОБЫ ===\n{PINK}ждите пока я добавлю доп жалобы :){RESET}")
+def dop():
+    clear()
+    print(f"{GRAY}―――――――――――――――――――\n    ДОП. ЖАЛОБЫ\n―――――――――――――――――――\n{PINK}[1]  воровство{RESET}")
     enters()
 
 def enters():
     input("Нажмите Enter чтобы вернуться в меню...")
 
 while True:
-    user_input = input(f"{CYAN}Введите 1 для запуска команды: {RESET}")
+    user_input = input(f"{CYAN}Введите 1 для запуска команды:{RESET} ")
     if user_input == "1":
         print(f"{GREEN}snoser{RESET}")
         break
@@ -48,44 +47,42 @@ while True:
         for _ in range(5):
             print(f"{RED}error. Введите 1{RESET}")
 
-valid_choices = ["1", "2", "3", "4", "5", "9", "0"]
+valid = ["1", "2", "3", "4", "5", "9", "0"]
 
 while True:
-    clear_screen()
-    show_menu()
+    clear()
+    menu()
     
-    choice = input(f"\n{CYAN}выберите пункт (1-9): {RESET}")
-    if choice not in valid_choices:
-
+    choice = input("\nвыберите пункт (1-9): ")
+    if choice not in valid:
         input(f"{RED}error. Введите одно из девяти\n{RESET}Нажмите Enter чтобы перезапустить графический интерфейс пользователя...")
         continue
 
     if choice == "1":
-        clear_screen()
+        clear()
         print(f"\n{GREEN}вы выбрали: snos{RESET}\n{BLUE}чтобы просто снести видео > пожаловаться > безопасность несовершеннолетних >\nнанесение вреда несовершеннолетних\n{RESET}Описание жалобы:\n\nданное видео содержит недопустимые моменты,\nэто может быть рассмотрено как нанесение вреда несовершеннолетним\n\n{RED}РЕДКО РАБОТАЕТ !!!{RESET}\n")
         enters()
 
     elif choice == "2":
-        clear_screen()
+        clear()
         print(f"{GREEN}вы выбрали: порнография{RESET}\n{BLUE}пожаловаться > порнография или нагота *выберите причину в зависимости от случая*\n{RESET}Описание жалобы:\n\nна данном видео присутсвует мужчина\женщина которая\nнаводит сексуальную атмосферу\n")
         enters()
         
     elif choice == "3":
-        clear_screen()
+        clear()
         print(f"\n{GREEN}вы выбрали: незаконные действия{RESET}\n{BLUE}пожаловаться > угроза личной безопасности\n{RESET}Описание жалобы:\n\nданное видео содержит незаконные действия, такие\nкак *описываете действия которые могут быть\nнезаконными*\n")
         enters()
     
     elif choice == "4":
-        show_about()
+        info()
 
     elif choice == "5":
-        show_dop()
+        dop()
 
     elif choice == "9":
-        show_rank()
+        rank()
         
     elif choice == "0":
         print(f"\n{RED}Выход из программы...{RESET}")
         time.sleep(5)
         exit()
-
